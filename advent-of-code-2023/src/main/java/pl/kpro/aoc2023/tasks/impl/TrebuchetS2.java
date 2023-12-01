6eb7ab0bddd41c4f64c65c865cc54a3aefca750f
Krzysztof Prorok <Krzysztof1397@gmail.com>
2023-12-01 22:37:05 +0100

    Readme

---
+++ advent-of-code-2023/src/main/java/pl/kpro/aoc2023/tasks/impl/TrebuchetS2.java
@@ -0,0 +1,49 @@
+package pl.kpro.aoc2023.tasks.impl;
+
+import pl.kpro.aoc2023.tasks.AdventTask;
+
+import java.util.Arrays;
+
+public class TrebuchetS2 implements AdventTask {
+    protected static final String NAME = "TrebuchetS2";
+    protected static final int NUMBER = 1;
+
+
+    @Override
+    public String getName() {
+        return NAME;
+    }
+
+    @Override
+    public int getNumber() {
+        return NUMBER;
+    }
+
+    @Override
+    public String run(String input) {
+        return Integer.toString(Arrays.stream(input.split("\n")).map(this::findNumber).mapToInt(Integer::parseInt).sum());
+    }
+
+    protected String findNumber(String line) {
+        char[] characters = line.toCharArray();
+        return new String(new char[]{this.findFirstDigitIn(characters), this.findLastDigit(characters)}).trim();
+    }
+
+    protected char findFirstDigitIn(char[] line) {
+        for (char c : line) {
+            if ('0' < c && c <= '9') {
+                return c;
+            }
+        }
+        return ' ';
+    }
+
+    protected char findLastDigit(char[] line) {
+        for (int i = 1; i <= line.length; i++) {
+            if ('0' <= line[line.length - i] && line[line.length - i] <= '9') {
+                return line[line.length - i];
+            }
+        }
+        return ' ';
+    }
+}
