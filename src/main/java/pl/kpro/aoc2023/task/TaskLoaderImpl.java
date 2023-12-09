2c0e7af3917c0ee281c2376663b3d88985f60ba6
Krzysztof Prorok <Krzysztof1397@gmail.com>
2023-12-09 11:15:05 +0100

    Day 9 soultions

--- src/main/java/pl/kpro/aoc2023/task/TaskLoaderImpl.java
+++ src/main/java/pl/kpro/aoc2023/task/TaskLoaderImpl.java
@@ -3,6 +3,7 @@ package pl.kpro.aoc2023.task;
 import org.reflections.Reflections;
 
 import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Modifier;
 import java.util.Comparator;
 import java.util.LinkedHashSet;
 import java.util.Set;
@@ -14,7 +15,7 @@ public class TaskLoaderImpl implements TaskLoader {
     public Set<AdventTask> getTasks() {
         Reflections reflections = new Reflections("pl.kpro.aoc2023.task");
         Set<Class<? extends AdventTask>> taskImplementations = reflections.getSubTypesOf(AdventTask.class);
-        return taskImplementations.stream().map(this::createInstance).sorted(Comparator.comparing(AdventTask::getNumber)).collect(Collectors.toCollection(LinkedHashSet<AdventTask>::new));
+        return taskImplementations.stream().filter(c -> !Modifier.isAbstract(c.getModifiers())).map(this::createInstance).sorted(Comparator.comparing(AdventTask::getNumber)).collect(Collectors.toCollection(LinkedHashSet<AdventTask>::new));
     }
 
     private AdventTask createInstance(Class<? extends AdventTask> taskImpl) {
